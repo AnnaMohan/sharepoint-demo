9954afbb5bd0af115b6fad9454ec37b3258b8ad1
AnnaMohan <annamohan3@gmail.com>
2021-08-09 20:34:54 +0530

    initial commit fixes AB#3

---
+++ src/webparts/share/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IShareWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'ShareWebPartStrings' {
+  const strings: IShareWebPartStrings;
+  export = strings;
+}
